57940f24eb0bf9e2b44dbdd9e92c156579ae03eb
Arianne Deaniele <100850374+daneeeeeee@users.noreply.github.com>
2025-02-23 14:29:17 +0800

    firebase

--- saltik/lib/screens/home_page.dart
+++ saltik/lib/screens/home_page.dart
@@ -15,7 +15,7 @@ class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    const ReservoirPage(),
+    ReservoirPage(),
     const PondPage(),
     const HistoryPage(),
     const ProfilePage(),
